906783a5be2c0b88ff26c650b8a5149340d69df4
Tasnimul Hasan <2408020@student.ruet.ac.bd>
2026-05-14 05:14:41 +0000

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,38 @@
-import { useState } from "react";
-import ColorDemo from "./components/ColorDemo";
-import ShapeDemo from "./components/ShapeDemo";
-import SizeDemo from "./components/SizeDemo";
+import { Suspense, useState } from "react";
+import demos from "./data/demos";
+import importDemo from "./utils/importDemo";
 
 export default function App() {
-    const [selectedDemo, setSelectedDemo] = useState(null);
+  const [selectedDemo, setSelectedDemo] = useState(null);
 
-    const selectDemo = (type) => {
-        setSelectedDemo(type);
-    };
+  const loadDemo = async (file) => {
+    const Demo = await importDemo(file);
+    return <Demo />;
+  };
 
-    return (
+  const selectDemo = async (file) => {
+    const DemoComponent = await loadDemo(file);
+
+    setSelectedDemo(DemoComponent);
+  };
+
+  return (
+    <div>
+      <div>
+        <h1>React Lazy load explained</h1>
+
         <div>
-            <div>
-                <h1>React Lazy load explained</h1>
-
-                <div>
-                    <button onClick={() => selectDemo("shape")}>
-                        Shape Demo
-                    </button>
-                    <button onClick={() => selectDemo("color")}>
-                        Color Demo
-                    </button>
-                    <button onClick={() => selectDemo("size")}>
-                        Size Demo
-                    </button>
-                </div>
-
-                <div>
-                    {selectedDemo === "shape" && <ShapeDemo />}
-                    {selectedDemo === "color" && <ColorDemo />}
-                    {selectedDemo === "size" && <SizeDemo />}
-                </div>
-            </div>
+          {demos.map((demo) => (
+            <button key={demo.name} onClick={() => selectDemo(demo.file)}>
+              {demo.name}
+            </button>
+          ))}
         </div>
-    );
+
+        <div>
+          <Suspense fallback={<p>Loading...</p>}>{selectedDemo}</Suspense>
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ src/data/demos.js
@@ -0,0 +1,19 @@
+const demos = [
+  {
+    id: 1,
+    name: "Color Demo",
+    file: "ColorDemo",
+  },
+  {
+    id: 2,
+    name: "Shape Demo",
+    file: "ShapeDemo",
+  },
+  {
+    id: 3,
+    name: "Size Demo",
+    file: "SizeDemo",
+  },
+];
+
+export default demos;
