f79192601a149a309cf5c43c1f6a1d6f78a83e20
Tasnimul Hasan <2408020@student.ruet.ac.bd>
2026-05-14 06:15:45 +0000

    done

--- src/App.jsx
+++ src/App.jsx
@@ -13,24 +13,25 @@ import ScrollReveal from "./components/ScrollReveal";
 import { AnimatePresence } from "framer-motion";
 
 function App() {
-    const location = useLocation();
-    return (
-        <>
-            <AnimatePresence mode="wait">
-                <Routes location={location} key={location.key}>
-                    <Route path="/" element={<Home />} exact />
-                    <Route path="/simple" element={<Simple />} />
-                    <Route path="/keyframe" element={<Keyframe />} />
-                    <Route path="/button-tap" element={<ButtonTap />} />
-                    <Route path="/text-motion" element={<TextMotion />} />
-                    <Route path="/transition" element={<TransitionType />} />
-                    <Route path="/counter" element={<Counter />} />
-                    <Route path="/scroll" element={<ScrollReveal />} />
-                    <Route path="/variants" element={<Variants />} />
-                </Routes>
-            </AnimatePresence>
-        </>
-    );
+  const location = useLocation();
+  return (
+    <>
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.key}>
+          <Route path="/" element={<Home />} exact>
+            <Route path="/simple" element={<Simple />} />
+            <Route path="/keyframe" element={<Keyframe />} />
+            <Route path="/button-tap" element={<ButtonTap />} />
+            <Route path="/text-motion" element={<TextMotion />} />
+            <Route path="/transition" element={<TransitionType />} />
+            <Route path="/counter" element={<Counter />} />
+            <Route path="/scroll" element={<ScrollReveal />} />
+            <Route path="/variants" element={<Variants />} />
+          </Route>
+        </Routes>
+      </AnimatePresence>
+    </>
+  );
 }
 
 export default App;
